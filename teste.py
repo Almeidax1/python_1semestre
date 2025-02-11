93370a18fa88615411feea58b411c1a5a14d8023
Almeida <156235628+Almeidax1@users.noreply.github.com>
2025-02-11 20:04:26 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,16 @@
+##Almeida.
+kaua = "Ola" #string
+
+print(kaua)
+
+idade = 18 #int
+
+print("minha idade", idade)
+
+nome = input("What is your name" ) 
+
+print("Adorei conversar com voce, " + nome + "!" ) 
+
+cor_favorita = input("Qual sua cor favorita? ") 
+
+print(nome + " Eu gosto da cor " + cor_favorita + ".")
